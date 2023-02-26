7f5c2f0a8919b2df1906ebfa0d536eef5aa346ce
Your name <dreyer.wil@gmail.com>
2023-02-26 17:02:51 +0100

    newportfolio

--- script.js
+++ script.js
@@ -1,102 +1,25 @@
-const contentData = [
-  {page : 0, navText : "WillDev", content :"William Dev" },
-  {page : 1, navText : "About me", content :"Je suis un dev gnagnagna" },
-  {page : 2, navText : "Project", content :"Mes derniers projects incroyables" },
-  {page : 3, navText : "Stack", content :"Maîtrise CSS fullstack t'as vu" },
-  {page : 4, navText : "Contact me", content :"contact@williamdev.fr" },
-]
+var initialPosition = 0;
+var currentPosition = initialPosition;
+var introductionText = document.getElementById('introductionText');
 
-let navActual = 0 ;
-
-
-let letters = document.querySelectorAll('.letter');
-let delay = 0;
-
-letters.forEach(function(letter) {
-  letter.style.animationDelay = delay + "s";
-  delay += 0.2;
+window.addEventListener('wheel', function(event) {
+  event.preventDefault();
+  
+  var deltaY = event.deltaY;
+  var step = 500; // Nombre de pixels par étape
+  var maxHeight = window.innerHeight - introductionText.offsetHeight + 1500; // Valeur maximale de la position
+  
+  currentPosition += deltaY > 0 ? step : -step; // Met à jour la position de défilement
+  
+  if (currentPosition < 0) {
+    currentPosition = 0; // Limite la position minimale de la div
+  } else if (currentPosition > maxHeight) {
+    currentPosition = maxHeight; // Limite la position maximale de la div
+  }
+  
+  // Calculer la proportion de la position de défilement actuelle par rapport à la hauteur totale de la fenêtre
+  var proportion = currentPosition / window.innerHeight;
+  
+  // Modifier la position de la div en utilisant la proportion
+  introductionText.style.transform = 'translateY(' + proportion * -100 + '%)';
 });
-
-
-console.log(navActual)
-
-  document.addEventListener("keydown", function(event) {
-    switch (event.key) {
-      case "ArrowLeft":
-        if (navActual === 0 || navActual === 3 || navActual === 4) {
-        document.querySelector("#center").innerHTML = contentData[1].content ;
-        navActual = 1 ;
-        console.log(navActual)
-      }
-        else if (navActual === 1) {
-        document.querySelector("#center").innerHTML = contentData[2].content ;
-        navActual = 2
-        console.log(navActual)
-        }
-       else  if (navActual === 2) {
-          document.querySelector("#center").innerHTML = contentData[0].content ;
-          navActual = 0
-          console.log(navActual)
-          }
-
-        break;
-      case "ArrowUp": 
-      if (navActual === 0 || navActual === 1 || navActual === 2) {
-        document.querySelector("#center").innerHTML = contentData[3].content ;
-        navActual = 3 ;
-        console.log(navActual)
-      }
-        else if (navActual === 3) {
-        document.querySelector("#center").innerHTML = contentData[4].content ;
-        navActual = 4
-        console.log(navActual)
-        }
-       else  if (navActual === 4) {
-          document.querySelector("#center").innerHTML = contentData[0].content ;
-          navActual = 0
-          console.log(navActual)
-          }
-
-        break;
-      case "ArrowDown": 
-      if (navActual === 0 || navActual === 1 || navActual === 2) {
-        document.querySelector("#center").innerHTML = contentData[4].content ;
-        navActual = 4 ;
-        console.log(navActual)
-      }
-        else if (navActual === 4) {
-        document.querySelector("#center").innerHTML = contentData[3].content ;
-        navActual = 3
-        console.log(navActual)
-        }
-       else  if (navActual === 3) {
-          document.querySelector("#center").innerHTML = contentData[0].content ;
-          navActual = 0
-          console.log(navActual)
-          }
-
-      break;
-      case "ArrowRight":
-        if (navActual === 0 || navActual === 3 || navActual === 4) {
-          document.querySelector("#center").innerHTML = contentData[2].content ;
-          navActual = 2 ;
-          console.log(navActual)
-        }
-          else if (navActual === 2) {
-          document.querySelector("#center").innerHTML = contentData[1].content ;
-          navActual = 1
-          console.log(navActual)
-          }
-         else  if (navActual === 1) {
-            document.querySelector("#center").innerHTML = contentData[0].content ;
-            navActual = 0
-            console.log(navActual)
-            }
-
-
-
-        break;
-    }
-  });
-
-
